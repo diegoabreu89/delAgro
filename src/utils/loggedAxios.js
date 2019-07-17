e594319eb851bccde9061c610f4fad366757e909
German Gardiol <german@asapuruguay.com>
2019-07-17 10:50:03 -0300

    Send deviceId in required logged endpoints

--- src/utils/loggedAxios.js
+++ src/utils/loggedAxios.js
@@ -1,12 +1,30 @@
+import firebase from 'react-native-firebase';
 import axios from 'axios';
 
-const instance = ({ token, uid, client }) => axios.create({
-  baseURL: 'http://delagro-api.herokuapp.com/api/v1/',
-  headers: {
-    'access-token': token,
-    client,
-    uid,
-  },
-});
+const instance = ({ token, uid, client }) => {
+  firebase.messaging().getToken()
+    .then((fcmToken) => {
+      if (fcmToken) {
+        axios.create({
+          baseURL: 'http://delagro-api.herokuapp.com/api/v1/',
+          headers: {
+            'access-token': token,
+            client,
+            uid,
+            device_token: fcmToken,
+          },
+        });
+      } else {
+        axios.create({
+          baseURL: 'http://delagro-api.herokuapp.com/api/v1/',
+          headers: {
+            'access-token': token,
+            client,
+            uid,
+          },
+        });
+      }
+    });
+};
 
 export default instance;
